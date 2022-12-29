b516bb8d946211cd9ed3ad63d1891061f0609206
ifty17 <iftyrahatkabir@gmail.com>
2022-12-29 09:00:22 +0600

    added update profile api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -27,28 +27,68 @@ try{
   const postCollection = client.db("dev-platform").collection("post");
 
 
-//Save post to database
-app.post('/post', async(req, res) => {
+
+  //get user by id
+  app.get("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const user = await usersCollection.findOne(query);
+    res.send(user);
+  });
+
+
+  //update user ifo api
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      const option = { upsert: true };
+      const updatedUser = {
+        $set: {
+          title: user.title,
+          institution: user.institution,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedUser,
+        option
+      );
+      res.send(result);
+    });
+
+  //Save post to database
+  app.post("/post", async (req, res) => {
     const post = req.body;
     const result = await postCollection.insertOne(post);
     res.send(result);
-})
+  });
 
-//get all post
-app.get('/posts', async(req, res) =>{
+  //get users
+  app.get("/users", async (req, res) => {
+    const email = req.query.email;
+    const query = {
+      email: email,
+    };
+    const result = await usersCollection.find(query).toArray();
+    res.send(result);
+  });
+
+  //get all post
+  app.get("/posts", async (req, res) => {
     const query = {};
     const cursor = postCollection.find(query);
-    const posts = await cursor.sort({dateField: -1}).toArray();
+    const posts = await cursor.sort({ dateField: -1 }).toArray();
     res.send(posts);
-})
+  });
 
-//get details post
-app.get("/post/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: ObjectId(id) };
-  const result = await postCollection.findOne(query);
-  res.send(result);
-});
+  //get details post
+  app.get("/post/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await postCollection.findOne(query);
+    res.send(result);
+  });
 
   // Save users to database
   app.post("/users", async (req, res) => {
